b6d4231d7ae9485a9e858a40a7e2d7dc19b76c44
Nate Nichols <nicho997@umn.edu>
2021-03-29 08:03:21 -0500

    changed the sound of the gun

--- Camera.gd
+++ Camera.gd
@@ -1,10 +1,9 @@
 extends Camera
 
 onready var cube = $"/root/Spatial/cube"
+onready var stream_player = $"AudioStreamPlayer"
 
-var rmb_audio = preload("res://sounds/deagle.wav")
-var lmb_audio = preload("res://sounds/pdudegun.wav")
-
+var audio_stream = preload("res://sounds/barrett.wav")
 
 var sound_3d = preload("res://audio/sound3d.tscn")
 var sound_direct = preload("res://audio/sound_direct.tscn")
@@ -13,18 +12,21 @@ var sound_direct = preload("res://audio/sound_direct.tscn")
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("lmb"):
 		cube.apply_central_impulse(Vector3(0,4,0))
 		var sound = sound_direct.instance()
 		add_child(sound)
-		sound.play_sound(lmb_audio)
+		sound.play_sound(audio_stream)
 
 	if Input.is_action_just_pressed("rmb"):
 		var sound = sound_3d.instance()
 		cube.add_child(sound)
-		sound.play_sound(rmb_audio)
+		sound.play_sound(audio_stream)
+
+	if Input.is_action_just_pressed("space"):
+		stream_player.play()
+
 
 	self.look_at_from_position(global_transform.origin, cube.global_transform.origin, Vector3.UP)
